541b06234adbb4f20825fd448875d4a3703746ef
Bruno Gambim <brunor.gambim@gmail.com>
2022-01-27 20:43:55 -0300

    implementação do endpoint comment criada

---
+++ controllers/comment_controller/comment_assembler.go
@@ -0,0 +1,18 @@
+package comment_controller
+
+import (
+	models "KleverTechnicalChallenge/domain/models"
+)
+
+func getCommentDtoFromModel(comment models.Comment) *GetCommentDTO {
+	return &GetCommentDTO{
+		Id:      comment.ID.Hex(),
+		Message: comment.Message,
+	}
+}
+
+func commentFromCreateDto(commentDto *CreateCommentDTO) models.Comment {
+	return models.Comment{
+		Message: commentDto.Message,
+	}
+}
---
+++ controllers/comment_controller/comment_controller_impl.go
@@ -0,0 +1,48 @@
+package comment_controller
+
+import (
+	"context"
+	"log"
+
+	services "KleverTechnicalChallenge/domain/services"
+
+	"github.com/golang/protobuf/ptypes/empty"
+)
+
+type CommentController struct {
+	UnimplementedCommentControllerServer
+	commentService *services.CommentService
+}
+
+func NewCommentController() *CommentController {
+	service, err := services.NewCommentService()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return &CommentController{
+		commentService: service,
+	}
+}
+
+func (controller *CommentController) FindAllComments(ctx context.Context, e *empty.Empty) (*GetAllCommentDTO, error) {
+	commentList, err := controller.commentService.FindAll()
+	if err != nil {
+		log.Printf(err.Error())
+		return &GetAllCommentDTO{}, nil
+	}
+	response := GetAllCommentDTO{}
+	for _, comment := range commentList {
+		response.Comments = append(response.Comments, getCommentDtoFromModel(comment))
+	}
+	return &response, nil
+}
+
+func (controller *CommentController) CreateComment(ctx context.Context, commentDto *CreateCommentDTO) (*empty.Empty, error) {
+	comment := commentFromCreateDto(commentDto)
+	_, err := controller.commentService.Insert(comment)
+	if err != nil {
+		log.Printf(err.Error())
+	}
+	return &empty.Empty{}, nil
+}
