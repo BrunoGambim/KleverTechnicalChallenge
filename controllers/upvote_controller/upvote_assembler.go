0c14be24bf96da204b94d954e23e0745af0b68e1
Bruno Gambim <brunor.gambim@gmail.com>
2022-01-28 10:25:35 -0300

    metodo get do endpoint upvote retorna data formatada

--- controllers/upvote_controller/upvote_assembler.go
+++ controllers/upvote_controller/upvote_assembler.go
@@ -2,6 +2,8 @@ package upvote_controller
 
 import (
 	"KleverTechnicalChallenge/domain/models"
+	"fmt"
+	"time"
 
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
@@ -15,9 +17,14 @@ func upvoteFromCreateDto(upvoteDto *CreateUpvoteDTO) (models.Upvote, error) {
 }
 
 func getAlbumDtoFromModel(upvote models.Upvote) *GetUpvoteDTO {
+	createdAtTime := time.Unix(int64(upvote.CreatedAt), 0)
+	createdAt := fmt.Sprintf("%d-%02d-%02d %02d:%02d:%02d",
+		createdAtTime.Year(), createdAtTime.Month(), createdAtTime.Day(),
+		createdAtTime.Hour(), createdAtTime.Minute(), createdAtTime.Second())
 	return &GetUpvoteDTO{
 		Id:        upvote.Id.Hex(),
 		Type:      upvote.Type,
 		CommentId: upvote.Id.Hex(),
+		CreatedAt: createdAt,
 	}
 }
